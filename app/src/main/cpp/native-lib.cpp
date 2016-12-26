b3a0cd93262e9f83683166d6f15842b37cde9263
BenNG <benjamin.kim.nguyen@gmail.com>
2016-12-26 18:08:32 +0100

    successfully display the name of the file in src/main/assets

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,8 @@
 #include <jni.h>
 #include <string>
+#include <android/asset_manager_jni.h>
+#include <android/asset_manager.h>
+
 
 extern "C"
 jstring
@@ -8,4 +11,60 @@ Java_moulinet_tech_moulinet_1sudoku_1app_MainActivity_stringFromJNI(
         jobject /* this */) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
+}
+
+
+
+extern "C"
+jobjectArray
+Java_moulinet_tech_moulinet_1sudoku_1app_MainActivity_getFilenameInAssets(
+        JNIEnv* env,
+        jobject javaThis,
+        jobject pAssetManager)
+{
+
+    // Number of file
+    const int maxNumFile = 20;
+    int numFile = 0;
+
+    // Asset manager
+    const char* filename;
+    AAssetManager* assetManager = AAssetManager_fromJava(env, pAssetManager);
+    AAssetDir* assetFolder = AAssetManager_openDir(assetManager, "");
+
+    // Put all file name into temporary array
+    jobjectArray tempArr = (jobjectArray)env->NewObjectArray(
+            maxNumFile,
+            env->FindClass("java/lang/String"),
+            env->NewStringUTF(""));
+
+    while (
+            ((filename = AAssetDir_getNextFileName(assetFolder)) != NULL) &&
+            (numFile < maxNumFile)
+            )
+    {
+        env->SetObjectArrayElement(
+                tempArr,
+                numFile,
+                env->NewStringUTF(filename));
+        numFile++;
+    }
+
+    // Copy file name from temporary array to returned array
+    jobjectArray returnedArr = (jobjectArray)env->NewObjectArray(
+            numFile,
+            env->FindClass("java/lang/String"),
+            env->NewStringUTF(""));
+    for (int i = 0; i < numFile; ++i)
+    {
+        jstring jstr = (jstring)env->GetObjectArrayElement(tempArr, i);
+        env->SetObjectArrayElement(returnedArr, i, jstr);
+    }
+
+    // Close assets folder
+    AAssetDir_close(assetFolder);
+
+    // Return
+    return returnedArr;
+
 }
--- app/src/main/java/moulinet/tech/moulinet_sudoku_app/MainActivity.java
+++ app/src/main/java/moulinet/tech/moulinet_sudoku_app/MainActivity.java
@@ -1,7 +1,9 @@
 package moulinet.tech.moulinet_sudoku_app;
 
+import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,11 +21,21 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+
+        String[] filenames = getFilenameInAssets(getAssets());
+
+        System.out.println(filenames);
+        Log.v("LOL", filenames[0]);
+
     }
+
+
+    private native String[] getFilenameInAssets(AssetManager pAssetManager);
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
 }
